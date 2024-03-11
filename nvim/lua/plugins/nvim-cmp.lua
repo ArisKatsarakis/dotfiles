25fee33e06b661b998855d8898314d8a95c01b8c
ArisKatsarakis <ariskatsarakis@hotmail.com>
2024-03-11 18:22:41 +0200

    Adding configs for cmp and setting new color scheme

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,19 @@
+function leave_snippet()
+  if
+    ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+    and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+    and not require("luasnip").session.jump_active
+  then
+    require("luasnip").unlink_current()
+  end
+end
+
+return {
+
+  { "hrsh7th/nvim-cmp" },
+  {
+    opts = vim.api.nvim_command([[autocmd ModeChanged * lua leave_snippet()]]),
+
+    -- stop snippets when you leave to normal mode
+  },
+}
